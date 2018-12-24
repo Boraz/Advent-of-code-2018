bd6a390a12bf5744d92914d21b9447e608d1c3c0
Boraz <Maxson.Daniel@gmail.com>
2018-12-24 15:34:31 -0500

    Puzzle 2 part 1 solution
    was double setting bools to true /facepalm

--- puzzle_02.py
+++ puzzle_02.py
@@ -1,42 +1,58 @@
 #!/usr/bin/env python
 
-# Iterate over the characters in a line and count duplicates and triplicates
+#  iterate over the characters in a line and count duplicates and triplicates
 def set_match():
-  double = False
+  doubleBool = False
+  trippleBool = False
   doubles = 0
-  tripple = False
   tripples = 0
   seen = set()
+  doubleSet = set()
+  trippleSet = set()
   with open("puzzle_input_02.txt") as f:
     while True:
       c = f.read(1)
       if not c:
         print("End of file")
         break
-      while c not in seen:
-        if c == '\n':
-          break
+      #  do all the calculations at the end of each line
+      if c == '\n':
+        if len(doubleSet) > 0:
+          doubleBool = True
+        if len(trippleSet) > 0:
+          trippleBool = True
+        if doubleBool is True:
+          doubles += 1
+        if trippleBool is True:
+          tripples += 1
+        doubleSet.clear()
+        trippleSet.clear()
+        seen.clear()
+        trippleBool = False
+        doubleBool = False
+        continue
+      #  no code for removing quad or greater
+      #  if c exists in double, add it to tripple and remove it from double
+      elif c in doubleSet:
+        trippleSet.add(c)
+        doubleSet.discard(c)
+      #  if c exists in the line already, add it to double
+      elif c in seen:
+        doubleSet.add(c)
+      #  if we have not seen c add it to the set
+      else:
         seen.add(c)
-      #if letter in set:
-       # if double is True:
-        #  tripple = True
-         # double = False
-      #if double:
-       # doubles += 1
-        #double = False
-      #if tripple:
-       # tripples += 1
-        #tripple = False
-  #print(doubles * tripples)
-  print(seen)
 
+  print("doubles ", doubles)
+  print("tripples ", tripples)
+  print(doubles * tripples)
 
-# Gather our code in a main() function
+#  gather our code in a main() function
 def main():
 
   print("Puzzle 2")
   set_match()
 
-# Standard boilerplate to call the main() function
+#  standard boilerplate to call the main() function
 if __name__ == '__main__':
   main()
